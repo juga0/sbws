9893e1d6e27f53cf16a06d76290f5f3996c3ad60
juga0 <juga@riseup.net>
2019-02-16 17:06:22 +0000

    Revert "scanner: store result on callback error"
    This reverts commit cf3e869aa40311feb32597bcced2e8edcc9ab6c1.
    
    The errors raised in callback_error are programming errors since
    any exception should have created already a Result or ignored.
    It will be better to realize whether new exceptions are raised
    while programming that to discover them time later in the bandwidth
    files.
    The possible errors at the moment have tickets and there should
    not be created new ones.
    Callback_error should be also removed to raise a possible exception
    with the full trace.

--- sbws/core/scanner.py
+++ sbws/core/scanner.py
@@ -381,16 +381,14 @@ def result_putter(result_dump):
     return closure
 
 
-def result_putter_error(result_dump, target):
+def result_putter_error(target):
     ''' Create a function that takes a single argument -- an error from a
     measurement -- and return that function so it can be used by someone else
     '''
     def closure(object):
         # The only object that can be here if there is not any uncatched
         # exception is stem.SocketClosed when stopping sbws
-        log.debug("%s %s", target, type(object))
-        r = [ResultError(target, [], '', '', msg=str(type(object)))]
-        result_dump.queue.put(r, timeout=1)
+        log.debug(type(object))
     return closure
 
 
@@ -454,7 +452,7 @@ def main_loop(args, conf, controller, relay_list, circuit_builder, result_dump,
             num_relays += 1
             # callback and callback_err must be non-blocking
             callback = result_putter(result_dump)
-            callback_err = result_putter_error(result_dump, target)
+            callback_err = result_putter_error(target)
             async_result = pool.apply_async(
                 dispatch_worker_thread,
                 [args, conf, destinations, circuit_builder, relay_list,
